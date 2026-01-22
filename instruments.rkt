2e07634b5b84cdd631c161c182454ec750a8c4a5
Kevin Haines <5947118+kbhaines@users.noreply.github.com>
2026-01-22 09:53:17 +0000

    add instruments

---
+++ instruments.rkt
@@ -0,0 +1,37 @@
+#lang racket
+
+(provide (all-defined-out))
+
+(define flute-1 "Flute 1")
+(define flute-2 "Flute 2")
+
+(define clarinet-1 "Clarinet 1")
+(define clarinet-2 "Clarinet 2")
+
+(define oboe-1 "Oboe 1")
+(define oboe-2 "Oboe 2")
+
+(define cor-anglais "Cor Anglais")
+
+(define bassoon-1 "Bassoon 1")
+(define bassoon-2 "Bassoon 2")
+
+(define trumpet-1 "Trumpet 1")
+(define trumpet-2 "Trumpet 2")
+(define trumpet-3 "Trumpet 3")
+
+(define horn-1 "Horn 1")
+(define horn-2 "Horn 2")
+(define horn-3 "Horn 3")
+(define horn-4 "Horn 4")
+
+(define trombone-1 "Trombone 1")
+(define trombone-2 "Trombone 2")
+(define trombone-3 "Trombone 3")
+
+(define violins-1 "Violins 1" )
+(define violins-2 "Violins 2" )
+(define violas "Violas")
+(define cellos "Cello")
+(define double-bass "Double Bass")
+
